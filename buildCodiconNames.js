28c605fe4c663356e20b5990897543b0e18b1cee
Abrifq <arda.aydin@operationsilkscarf.com>
2026-01-15 17:10:03 +0300

    Update buildCodiconNames.js
    Since https://github.com/microsoft/vscode-codicons/commit/13c105213946c21c694cc22eeaa738ba19982407 codicon name/id order has changed.

--- buildCodiconNames.js
+++ buildCodiconNames.js
@@ -6,4 +6,9 @@ var cp = require( "child_process" );
 var raw = cp.execSync( "curl -s https://raw.githubusercontent.com/microsoft/vscode-codicons/main/src/template/mapping.json" ).toString();
 var mappings = JSON.parse( raw );
 
-fs.writeFileSync( 'src/codiconNames.js', "module.exports = " + JSON.stringify( Object.keys( mappings ), null, 2 ) + ";\n" );
+var codicons = new Set();
+for(const codiconID in mappings)
+    for (const codiconName of mappings[codiconID]) 
+        codicons.add(codiconName);
+
+fs.writeFileSync( 'src/codiconNames.js', "module.exports = " + JSON.stringify( Array.from(codicons), null, 2 ) + ";\n" );
